935e0e21188890aeba35d9632ac2a99d95c8fc88
Paul Moore <pdmoore@gmail.com>
2019-09-05 10:29:02 -0700

    example 3 - 100% line coverage with 3 tests

--- Java/src/test/java/com/gildedrose/Example_3_IncidentalCoverage.java
+++ Java/src/test/java/com/gildedrose/Example_3_IncidentalCoverage.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertEquals;
 
 public class Example_3_IncidentalCoverage {
 
+    // Add tests to cover the last line of code/most indented in the method
+    // Continue til 100% line coverage
 
     @Test
     public void AgedBrie_Past_SellinDate() {
@@ -14,7 +16,7 @@ public class Example_3_IncidentalCoverage {
         sut.updateQuality();
         assertEquals("Aged Brie, -1, 27", sut.items[0].toString());
     }
-    // First test: Line: 54%  Branch: 12%
+    // First test: Line 54%  Branch 12%
 
     @Test
     public void BackStagePass_Past_SellinDate() {
@@ -23,6 +25,17 @@ public class Example_3_IncidentalCoverage {
         sut.updateQuality();
         assertEquals("Backstage passes to a TAFKAL80ETC concert, -1, 0", sut.items[0].toString());
     }
-    // Second test: Line 81%  Branch: 28%
+    // Second test: Line 81%  Branch 28%
 
+    @Test
+    public void UnspecialItem() {
+        Item[] items = new Item[] { new Item("generic", 0, 10) };
+        GildedRose sut = new GildedRose((items));
+        sut.updateQuality();
+        assertEquals("generic, -1, 8", sut.items[0].toString());
+    }
+    // Third test: Line 100%  Branch 33%
+
+    // Only some lines are intentionally covered. Most are incidentally covered.
+    // For example, delete lines 24-34 and all tests still pass
 }
